79b11f70208326deeb14f1ce90ade660f817bd8c
alda <alda@kata.(none)>
2009-12-02 18:18:53 +0100

    First serious commit.

---
+++ sudoku.rb
@@ -0,0 +1,209 @@
+#!/usr/bin/ruby
+
+require 'sudoku_core.rb'
+
+require 'tk'
+
+class TkSudoku < Sudoku_core
+
+  Colors=["#daeaea", "#f8e7cd"] * 2
+  DialogParams = { "defaultextension" => "*.sudoku", "filetypes"=>[["Sudoku", [".sudoku"]], ["All files", ["*.*"]]] }
+
+  def initialize
+    init_variables
+
+    solve = proc { init_variables; update_sudoku; lockscreen; solve_sudoku; print; update_window } # print;  print; 
+    new   = proc { clear_all; unlockscreen }
+    open  = proc {
+      unlockscreen
+      if open_sudoku Tk.getOpenFile( DialogParams.update("title"=>"Open Sudoku") ) then
+        update_window
+      else
+        puts "error"
+      end
+    }
+    save = proc {
+      if ! save_sudoku Tk.getSaveFile( DialogParams.update("title"=>"Save Sudoku") ) then
+        puts "error"
+      end
+    }
+    prin = proc { begin; print; rescue; end }
+    prin_ver = proc { begin; print_verbose; rescue; end }
+
+    @root = TkRoot.new() { title "SUDOKU"; resizable  false, false } #; geometry '306x326' } # ; iconbitmap "sudoku.ico" } # +-position_x+-position_y
+
+    bar = TkMenu.new()
+    sys = TkMenu.new(bar) { tearoff false }
+    sys.add 'command', 'label'=>"New",  'underline'=>0, 'command' => new
+    sys.add 'command', 'label'=>"Open", 'underline'=>0, 'command' => open
+    sys.add 'command', 'label'=>"Save", 'underline'=>0, 'command' => save
+    sys.add 'separator'
+    sys.add 'command', 'label'=>"Quit", 'underline'=>0, 'command' => proc { @root.destroy }
+    
+    bar.add 'cascade', 'menu' => sys, 'label'=>"Menu", 'underline'=>0
+    bar.add 'command', 'label'=>"Solve", 'underline'=>0, 'command' => solve
+    bar.add 'command', 'label'=>"Print", 'underline'=>0, 'command' => prin
+    bar.add 'command', 'label'=>"Print!", 'underline'=>0, 'command' => prin_ver
+    @root.menu bar
+
+    @buttons = Array.new
+    @values = Array.new
+
+    #button = TkButton.new() { text "Vyresit sudoku"; command solve }
+    #button.pack "side" => "right" #, "fill"=>"y")
+
+    9.times { |a|
+      line = TkFrame.new#.pack 'expand'=>true
+      3.times { |f|
+        cell = TkFrame.new(line){ background Colors[(f%2)+(a/3)] } . pack "side"=>"left", 'expand'=>true
+        3.times { |g|
+          @values << TkVariable.new
+          @buttons << TkEntry.new(cell, 'textvariable' => @values[-1]) { width 2; justify "center" }
+          @buttons[-1].pack "side" => "left", 'expand' => true, "pady" => 5, "padx" => 4
+          @buttons[-1].font('9x15')
+
+          @buttons[-1].bind("Any-ButtonRelease") {
+            @buttons[9*a+3*f+g].cursor = 1
+          }
+          @buttons[-1].bind("Any-KeyRelease") { |event|
+            entries_any_key_event event, 9 * a + 3 * f + g
+            puts "anyrelease"
+          }
+          #@buttons[-1].bind("Any-KeyPress") { |event|
+          #   if @values[9 * a + 3 * f + g].value.size > 1 then
+          #     @values[9 * a + 3 * f + g].value = "R"
+          #   end
+          #}
+        }
+      }
+      line.pack# 'expand'=>true
+    }
+  end
+  
+  def clear_all
+    @values.each { |a|
+      a.value = ""
+    }
+  end
+
+  def update_window
+    9.times { |a|
+      9.times { |b|
+        if @sudoku[a][b].size == 1
+          @values[9*a+b].value = @sudoku[a][b][0]
+        else
+          @values[9*a+b].value = ""
+        end
+      }
+    }
+  end
+  
+  def update_sudoku
+    9.times { |a|
+      @sudoku[a] = Array.new
+      9.times { |b|
+        if @values[9*a+b].value != "" then
+          @sudoku[a][b] = [@values[9*a+b].value.to_i]
+        else
+          @sudoku[a][b] = [1,2,3,4,5,6,7,8,9]
+        end
+      }
+    }
+  end
+
+  def save_sudoku filename
+    begin
+      file = File.new filename, "w"
+      @values.each_index { |i|
+        if @values[i].value != "" then
+      file.write @values[i].value
+    else
+      file.write "0"
+    end
+        file.write "\n" if i % 9 == 8
+      }
+      file.close
+      return true
+    rescue
+      return false
+    end
+  end
+
+
+  private
+
+  def entries_any_key_event event, coord
+  if event.char >= "1" and event.char <= "9" then
+    @buttons[coord+1].focus
+    @buttons[coord+1].cursor = 1
+    if @values[coord].value.size > 0 then
+    @values[coord].value = @values[coord].value[-1].chr
+    end
+  elsif event.char.downcase != "" and event.char != "\t" then
+    @buttons[coord].value = ""
+  else
+    movecursor event.keysym, coord  #event a souradnice
+  end
+  #p event.mode
+  end
+
+  def movecursor keysym, coord
+  case keysym
+    when "Left"
+    @buttons[coord-1].focus
+    @buttons[coord-1].cursor = 1
+    when "Right"
+      coord -= 81 if coord+1 > 80
+    @buttons[coord+1].focus
+    @buttons[coord+1].cursor = 1
+    when "Down"
+      coord -= 81 if coord+9 > 80
+    @buttons[coord+9].focus
+    @buttons[coord+9].cursor = 1
+    when "Up"
+    @buttons[coord-9].focus
+    @buttons[coord-9].cursor = 1
+  end
+  end
+  
+  def lockscreen
+    @buttons.each_index { |i|
+      if @buttons[i].value.size == 1 then
+        @buttons[i].configure 'state' => 'disabled'
+      end
+    }
+  end
+  
+  def unlockscreen
+    @buttons.each_index { |i|   
+      @buttons[i].configure 'state' => 'normal'
+    }
+  end
+
+end
+
+
+sudoku = TkSudoku.new
+
+#p Tk.bindinfo_all
+#p TkSudoku.methods
+
+Tk.mainloop
+
+
+
+# puvodni button bind:
+=begin
+          @buttons[-1].bind("Any-KeyRelease") {
+            if @values[9*a+3*f+g].value.size > 1 then
+              @values[9*a+3*f+g].value = @values[9*a+3*f+g].value[-1].chr
+            end
+            if @values[9*a+3*f+g].value < "1" || @values[9*a+3*f+g].value > "9" then
+              @values[9*a+3*f+g].value = ""
+              #@buttons[9*a+3*f+g].configure 'state' => 'disabled'
+            end
+            if 9*a+3*f+g+1 < 81 and @values[9*a+3*f+g].value != "" then
+              @buttons[9*a+3*f+g+1].focus
+            end
+          }
+=end
---
+++ sudoku_core.rb
@@ -0,0 +1,216 @@
+class Sudoku_core
+
+  def initialize
+    init_variables
+  end
+  
+  def open_sudoku filename
+    begin
+      file = File.new filename, "r"
+      puts "opened #{filename}"
+      9.times { |i|
+        @sudoku[i] = Array.new
+        9.times { |j|
+          char = file.getc#.chr
+          p char
+          char = char.chr
+          redo if char == "\n" || char == " " || char == "\r"
+          char = char.to_i
+          if char == 0
+            @sudoku[i][j] = [1,2,3,4,5,6,7,8,9]
+          else
+            @sudoku[i][j] = [char]
+          end
+        }
+      }
+      file.close
+      return true
+    rescue
+      return false
+    end
+  end
+
+
+  def init_variables
+    @cols = Array.new 9
+    @cols.each_index { |i|
+      @cols[i] = [1,2,3,4,5,6,7,8,9]
+    }
+
+    @rows = Array.new 9
+    @rows.each_index { |i|
+      @rows[i] = [1,2,3,4,5,6,7,8,9]
+    }
+
+    @cells = Array.new 3
+    @cells.each_index { |i|
+      @cells[i] = [ [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9] ]
+    }
+#    @cyc = 0
+    @sudoku = Array.new
+  end
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+  def solve_sudoku
+    check_rows
+    check_cols
+    check_cells
+    begin
+      sizep = @sudoku.flatten.size
+      check_rows
+      check_cols
+      check_cells
+    end while sizep > @sudoku.flatten.size
+  end
+  
+  def del_row row, val
+    @rows[row].delete val
+    9.times { |i|
+      @sudoku[row][i].delete val
+    }
+  end
+  
+  def del_col col, val
+    @cols[col].delete val
+    9.times { |i|
+      @sudoku[i][col].delete val
+    }
+  end
+  
+  def del_cell x, y, val
+    locx = x / 3
+    locy = y / 3
+    @cells[locx][locy].delete val
+    (locx*3).upto(locx*3+2) { |i|
+      (locy*3).upto(locy*3+2) { |j|
+         @sudoku[i][j].delete val
+      }
+    }
+  end
+
+  def check_rows
+    9.times { |row|
+      @rows[row].each { |num|
+        where = Array.new
+        9.times { |col|
+          where += [col] if @sudoku[row][col].include? num
+        }
+        where.each { |col|
+          if where.length == 1 or @sudoku[row][col] == [num]
+            del_row row, num
+            del_col col, num
+            del_cell row, col, num
+            @sudoku[row][col] = [num]
+            break
+          end
+        }
+      }
+    }
+  end
+
+  def check_cols
+    9.times { |col|
+      @cols[col].each { |num|
+        where = Array.new
+        9.times { |row|
+          where += [row] if @sudoku[row][col].include? num
+        }
+        where.each { |row|
+          if where.length == 1 or @sudoku[row][col] == [num] 
+            del_row row, num
+            del_col col, num
+            del_cell row, col, num
+            @sudoku[row][col] = [num]
+            break
+          end
+        }
+      }
+    }
+  end
+
+  def check_cells
+    3.times { |i|
+      3.times { |j|
+        @cells[i][j].each { |num|
+          a = Array.new
+          b = Array.new
+          3.times { |x|
+            3.times { |y|
+              if @sudoku[i*3+x][j*3+y].include? num
+                a += [i*3+x]
+                b += [j*3+y]
+              end
+            }
+          }
+          if a.length == 1
+            del_row a[0], num
+            del_col b[0], num
+            del_cell a[0], b[0], num
+            @sudoku[a[0]][b[0]] = [num]
+          else
+            if a.uniq.length == 1
+              9.times { |q|
+                next if @sudoku[a[0]][q].length == 1
+                z = @sudoku[a[0]][q].delete num if not b.include? q
+                if @sudoku[a[0]][q].length == 1
+                  tmp = @sudoku[a[0]][q][0]
+                  del_row a[0], tmp
+                  del_col q, tmp
+                  del_cell a[0], q, tmp
+                  @sudoku[a[0]][q] = [tmp]
+                end 
+              }
+            elsif b.uniq.length == 1
+              9.times { |q|
+                next if @sudoku[q][b[0]].length == 1
+                @sudoku[q][b[0]].delete num if not a.include? q
+                if @sudoku[q][b[0]].length == 1
+                  tmp = @sudoku[q][b[0]][0]
+                  del_row q, tmp
+                  del_col b[0], tmp
+                  del_cell q, b[0], tmp
+                  @sudoku[q][b[0]] = [tmp]
+                end
+              }
+            end
+          end
+        }
+      }
+    }
+  end
+  
+  
+  def print
+    9.times { |i|
+      9.times { |j|
+        if @sudoku[i][j].length == 1
+          STDOUT.print @sudoku[i][j].to_s.delete '[]'
+        else 
+          STDOUT.print '0'
+        end
+      }
+      puts
+    }
+    puts "\n"
+  end
+
+  def print_verbose
+    9.times { |i|
+      9.times { |j|
+        STDOUT.print @sudoku[i][j].to_s.delete('[, ]'), ' '
+      }
+      puts
+    }
+    puts "\n"
+  end
+
+end
+
+=begin
+s = Sudoku_core.new
+s.open_sudoku "E:/Prog/Ruby/TkSudoku/lehke.sudoku"
+s.print
+s.solve_sudoku
+s.print
+=end
